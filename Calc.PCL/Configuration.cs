02534aca667c6062bec1c519721c5f8e95ffd0f1
rehanalam <git@apimatic.io>
2017-04-17 11:45:52 +0000

    test msg

---
+++ Calc.PCL/Configuration.cs
@@ -0,0 +1,10 @@
+
+namespace Calc.PCL
+{
+    public partial class Configuration
+    {
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+    }
+}
